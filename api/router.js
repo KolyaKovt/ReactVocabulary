0796f02c0838c1b4aa5bc869719e8bf1151affc8
Kovtunets Mykola <kovtunets73@gmail.com>
2024-02-05 23:20:24 +0100

    the server is working

--- api/router.js
+++ api/router.js
@@ -26,10 +26,12 @@ db.serialize(() => {
   `)
 })
 
-function queryAsync(query) {
+function queryAsync(query, params = []) {
   return new Promise((resolve, reject) => {
-    db.all(query, (err, rows) => {
-      if (err) reject(err)
+    db.all(query, params, (err, rows) => {
+      if (err) {
+        reject(err)
+      }
       resolve(rows)
     })
   })
